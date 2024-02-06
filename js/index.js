cdbd62f9eef3582b958a7163b1ff3341ba534c54
Valeriia <valeriia.air@gmail.com>
2024-02-06 21:13:00 +0100

    random quotes + random rgb

---
+++ js/index.js
@@ -0,0 +1,41 @@
+import quotes from "../data/quotes.json" assert { type: "json" };
+console.log(quotes);
+
+//Function to randomly select a quote value and return a random quote object from the quotes array
+function getRandomQuote() {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomNumber];
+  return randomQuote;
+}
+
+//Function to select random rgb color value
+function getRandomColor() {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const randomColor = "rgb(" + red + "," + green + "," + blue + ")";
+  return randomColor;
+}
+
+//Function to call the getRandomQuote function and stores the returned quote object in a variable
+//Constructs a string containing the different properties of the quote object
+function printQuote() {
+  const quotes = getRandomQuote();
+  const quoteContainer = document.getElementById("quote-box");
+  let quoteString = `<p class="quote">${quotes.quote}</p><p class="source">${quotes.source}`;
+
+  quoteContainer.innerHTML = quoteString;
+
+  //assigns random color value to document background color
+  document.body.style.backgroundColor = getRandomColor();
+}
+
+//Quote automatically refreshes every 15 seconds
+// window.setInterval(function () {
+//   printQuote();
+// }, 15000);
+
+//Event listener on LoadQuote button to generate new quote
+document
+  .getElementById("loadQuote")
+  .addEventListener("click", printQuote, false);
